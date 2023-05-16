cf075255fd5d73c1599d0f6107abbd0abed0b9e8
Jack Roudebush <roudebushjack@gmail.com>
2023-05-15 22:21:19 -0400

    Capitalize Game in class definition

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,12 @@
-class game
+class Game
+  attr_reader :code, :rounds
+  
   def initialize
+    @code = []
     @rounds = 6
+  end
+
+  def code=(*)
+    @code = File.readlines('google-10000-english-no-swears.txt').sample
   end
 end
