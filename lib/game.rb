e360db4342db0ee731a79821caf2095410711d61
Jack Roudebush <roudebushjack@gmail.com>
2023-05-15 22:27:04 -0400

    Add functionality to convert the code to an array

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,11 @@
 class Game
-  attr_reader :code, :rounds
+  attr_accessor :code, :rounds
   
   def initialize
     @code = File.readlines('google-10000-english-no-swears.txt').sample
     @rounds = 6
   end
+
+  def convert_code(code_copy)
+    code_array = code.split('')
 end
