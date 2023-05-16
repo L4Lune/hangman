1431c245d124cb4d89dfa27d8a6df644506e8894
Jack Roudebush <roudebushjack@gmail.com>
2023-05-16 08:49:31 -0400

    Add display_code_board work in progress  method

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,24 @@
 class Game
-  attr_accessor :code, :rounds
+  attr_accessor :code, :rounds, :code_array
   
   def initialize
     @code = File.readlines('google-10000-english-no-swears.txt').sample
+    @code_array = []
     @rounds = 6
   end
 
-  def convert_code(code)
-    code_array = code.scan(/[a-z]/)
+  def code_array=(code)
+    @code_array = code.scan(/[a-z]/)
     p code_array
+  end
+
+  def display_code_board(code_array)
+    i = 0
+    code_board = []
+    until code_board.length == code_array.length do
+      code_board[i] == '_'
+      i+=1
+    end
+    p code_board
   end
 end
