3517ea41d50cc534ffe8c003e3ed2a8444be84e5
Jack Roudebush <roudebushjack@gmail.com>
2023-05-15 22:13:14 -0400

    Work on setter method input validation. Not finished

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,21 @@
 class Player
   attr_reader :guess, :name
   
+  def initialize
+    @guess = nil
+    @name = nil
+  end
+  
   def guess=(user_input)
     puts 'What letter will you select?'
-
-    @guess = gets.chomp
+    user_input = gets.chomp.downcase
+    if user_input.length > 1 || user_input.match(/\d/)
+      puts 'That is not a valid guess. Please enter a letter.' 
+      guess=()
+    else user_input.length == 1 && user_input.match(/[a-z]/)
+      @guess = user_input
+      p guess
+    end
   end 
 
   def name=(user_input)
