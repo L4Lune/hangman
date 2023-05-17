9c07c48ea6021573298f4c3e7cb722ca9d0efe72
Jack Roudebush <roudebushjack@gmail.com>
2023-05-16 20:44:24 -0400

    Rename game class to game_board and recreate a game class

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require_relative 'lib/player'
-require_relative 'lib/game'
+require_relative 'lib/game_board'
+require 'pry-byebug'
 
+binding.pry
+game = GameBoard.new
 player = Player.new
+game.code_array=(game.code)
+game.display_code_board(game.code_array)
 player.guess=()
+game.update_code_board(player.guess)
